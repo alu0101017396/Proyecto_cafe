bf0070e161cefb040f9305502ae00a271355cf47
Cristian Rodriguez <43813175+alu0101020730@users.noreply.github.com>
2021-01-21 16:57:39 +0000

    AÑO MAXIMO = AÑO ACTUAL - 14

--- Trigger/Triggers/TriggerComprobarFechaCliente.sql
+++ Trigger/Triggers/TriggerComprobarFechaCliente.sql
@@ -6,7 +6,9 @@ DELIMITER //
 
 CREATE TRIGGER fecha_cliente BEFORE INSERT ON Cliente FOR EACH ROW
 BEGIN
-    IF YEAR(NEW.Fecha_de_nacimiento) < 1900 THEN
+    DECLARE añomaximo integer;
+    SELECT YEAR(CURRENT_DATE) INTO añomaximo;
+    IF YEAR(NEW.Fecha_de_nacimiento) < 1900  || YEAR(NEW.Fecha_de_nacimiento) > (añomaximo - 14) THEN
          signal sqlstate '45000' set message_text = 'Una persona no puede tener ese año de nacimiento';
     END IF;
 END //
